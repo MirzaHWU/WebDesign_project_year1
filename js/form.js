1f5c84c4f0313ecebc696a776d750caebb40274d
Shaikh, Faraz Z <fzs3@hw.ac.uk>
2020-02-23 22:22:28 +0400

    removed all libraries

--- js/form.js
+++ js/form.js
@@ -1,3 +1,34 @@
+function $(selector) {
+
+  return document.querySelector(selector);
+} 
+
+window.dataStorage = {
+  _storage: new WeakMap(),
+  put: function (element, key, obj) {
+      if (!this._storage.has(element)) {
+          this._storage.set(element, new Map());
+      }
+      this._storage.get(element).set(key, obj);
+  },
+  get: function (element, key) {
+      return this._storage.get(element).get(key);
+  },
+  has: function (element, key) {
+      return this._storage.has(element) && this._storage.get(element).has(key);
+  },
+  remove: function (element, key) {
+      var ret = this._storage.get(element).delete(key);
+      if (!this._storage.get(element).size === 0) {
+          this._storage.delete(element);
+      }
+      return ret;
+  }
+}
+
+
+console.log($(".body"));
+
 const checkbox = document.querySelector('.main-contact-form-container .my-form input[type="checkbox"]');
 const btns = document.querySelectorAll(".main-contact-form-container .my-form button");
 
